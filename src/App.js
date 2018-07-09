5d1e9d0d0c77862b4b5f7b0a35bf17d9f3ea0492
Kate Pond <kate@thepondsedge.com>
2018-07-09 14:23:43 -0700

    creates a callback to send selected color to App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import Countdown from './components/Countdown';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color: {},
+    };
+  }
+
+  selectedColor = (color) => {
+    this.setState({
+      color: color,
+    });
+  }
+
   render() {
     return (
       <section>
         <header className="header">
           <h1 className="header__h1"><span className="header__text">Color Our Sign</span></h1>
         </header>
-        <ChooseColor />
+        <ChooseColor colorSelectionCallback={this.selectedColor}/>
       </section>
     );
   }
--- src/components/ChooseColor.js
+++ src/components/ChooseColor.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from '../assets/logo.svg';
 import './ChooseColor.css';
 import { CirclePicker } from 'react-color';
 
 class ChooseColor extends Component {
+  state = {
+    choice: { r: 51, g: 51, b: 51 },
+  };
+
+  handleChangeComplete = (color) => {
+    this.setState({ choice: color.rgb });
+  };
+
+  valid = () => {
+    if( (typeof this.state.choice === "object") && (this.state.choice !== null) ){
+    return true;
+    }
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (this.valid ()) {
+
+      this.props.colorSelectionCallback(this.state.choice);
+    }
+  }
 
   render() {
     return (
       <section className="landing">
-        <CirclePicker />
-        <img src={logo} className="choose-logo" alt="The Pond's Edge logo" />
+        <section>
+          <CirclePicker
+          color={ this.state.choice }
+          onChangeComplete={ this.handleChangeComplete }/>
+
+          <form onSubmit={this.onFormSubmit}>
+            <div className="sumbit-form">
+            <input className="sumbit-form__sumbit" type="submit" value="Change The Sign!" />
+            </div>
+          </form>
+        </section>
+
+        <footer className="landing__logo">
+          <img src={logo} className="choose-logo" alt="The Pond's Edge logo" />
+        </footer>
       </section>
     )
   }
 }
 
 export default ChooseColor;
+
+ChooseColor.propTypes = {
+  colorSelectionCallback: PropTypes.func.isRequired,
+};
