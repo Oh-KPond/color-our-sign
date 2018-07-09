1c2c97c71832c4e58972214426c2809b93839f6b
Kate Pond <kate@thepondsedge.com>
2018-07-09 11:30:23 -0700

    installs color picker react-color'

--- src/components/ChooseColor.js
+++ src/components/ChooseColor.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import './ChooseColor.css';
+import { CirclePicker } from 'react-color';
 
 class ChooseColor extends Component {
 
   render() {
     return (
       <section className="landing">
+        <CirclePicker />
         <img src={logo} className="choose-logo" alt="The Pond's Edge logo" />
       </section>
     )
